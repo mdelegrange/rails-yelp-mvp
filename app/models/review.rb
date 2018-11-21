d123d56e67d14367363e5d86d3a585fd54fe33d0
matthieu delegrange <mdelegrange@gmail.com>
2018-11-21 11:48:39 +0100

    Added models Restaurant and Review and validations

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :restaurant_id, :content, presence: true
+  validates :rating,
+            presence: true,
+            numericality: { only_integer: true },
+            inclusion: { in: [0, 1, 2, 3, 4, 5] }
+end
