478d047ca2de07c362605dff1a695d376fcc359f
matthieu delegrange <mdelegrange@gmail.com>
2018-11-21 14:29:28 +0100

    Yelp MVP finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.new(
+    name:         Faker::LordOfTheRings.location ,
+    address:      Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category:     %w[chinese italian japanese french belgian].sample
+  )
+  restaurant.save!
+end
+
+
+20.times do
+  review = Review.new(
+    content: Faker::Hipster.paragraph,
+    rating: [0, 1, 2, 3, 4, 5].sample,
+    restaurant_id: Restaurant.ids.sample
+    )
+  review.save!
+
+end
